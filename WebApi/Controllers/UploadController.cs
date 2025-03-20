82104cc2c191900383cd265e0dfac6e11882b030
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-20 14:20:34 +0700

    Function Api Upload Multiple

--- WebApi/Controllers/UploadController.cs
+++ WebApi/Controllers/UploadController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using WebApi.Controllers;
 using WebApi.Services;
+
+namespace WebApi.Controllers;
 
 [ApiController, Route("api/[controller]")]
 public class UploadController : BaseController
@@ -8,49 +9,12 @@ public class UploadController : BaseController
     [HttpPost]
     public string? Post(IFormFile f)
     {
-        if (f == null)
-        {
-            return null;
-        }
-
-        string ext = Path.GetExtension(f.FileName);
-        string fileName = Helper.RandomString(32 - ext.Length) + ext;
-        string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", fileName);
-
-        using (Stream stream = new FileStream(path, FileMode.Create))
-        {
-            f.CopyTo(stream);
-        }
-
-        return fileName;
+        return Helper.Upload(f);
     }
 
     [HttpPost("multiple")]
     public IEnumerable<string>? Post(IFormFile[] af)
     {
-        if (af is null)
-        {
-            return null;
-        }
-
-        string root = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
-        List<string> list = new List<string>(af.Length);
-
-        foreach (var f in af)
-        {
-            string ext = Path.GetExtension(f.FileName);
-            string fileName = Helper.RandomString(32 - ext.Length) + ext;
-            string path = Path.Combine(root, fileName);
-
-            using (Stream stream = new FileStream(path, FileMode.Create))
-            {
-                f.CopyTo(stream);
-            }
-
-            list.Add(fileName);
-        }
-
-        return list;
+        return Helper.Upload(af);
     }
-    
 }
--- WebApi/Services/Helper.cs
+++ WebApi/Services/Helper.cs
@@ -13,4 +13,49 @@ namespace WebApi.Services;
             }
             return string.Join(string.Empty, arr);
         }
+        public static string? Upload(IFormFile f, string root)
+        {
+            if (f is null)
+                return null;
+
+            string ext = Path.GetExtension(f.FileName);
+            string fileName = RandomString(32 - ext.Length) + ext;
+            string path = Path.Combine(root, fileName);
+
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                f.CopyTo(stream);
+            }
+
+            return fileName;
+        }
+
+        public static string? Upload(IFormFile f)
+        {
+            return Upload(f, Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images"));
+        }
+
+        public static IEnumerable<string>? Upload(IFormFile[] af, string root)
+        {
+            if (af is null)
+                return null;
+
+            List<string> list = new List<string>(af.Length);
+            foreach (var f in af)
+            {
+                string? fileName = Upload(f, root);
+                if (fileName != null)
+                {
+                    list.Add(fileName);
+                }
+            }
+
+            return list;
+        }
+
+        public static IEnumerable<string>? Upload(IFormFile[] af)
+        {
+            return Upload(af, Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images"));
+        }
+
     }
