3ce5e37daff6f01394a598fda92962025722d662
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-20 13:50:56 +0700

    Api Upload Multiple

--- WebApi/Controllers/UploadController.cs
+++ WebApi/Controllers/UploadController.cs
@@ -24,4 +24,33 @@ public class UploadController : BaseController
 
         return fileName;
     }
+
+    [HttpPost("multiple")]
+    public IEnumerable<string>? Post(IFormFile[] af)
+    {
+        if (af is null)
+        {
+            return null;
+        }
+
+        string root = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+        List<string> list = new List<string>(af.Length);
+
+        foreach (var f in af)
+        {
+            string ext = Path.GetExtension(f.FileName);
+            string fileName = Helper.RandomString(32 - ext.Length) + ext;
+            string path = Path.Combine(root, fileName);
+
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                f.CopyTo(stream);
+            }
+
+            list.Add(fileName);
+        }
+
+        return list;
+    }
+    
 }
--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfd5347c93f8ef97764fdd99851b2b6a2e94dc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fefe69ad578d1c0083fb7adfea77eae9f772d65b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
