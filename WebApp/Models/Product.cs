9ef45ae267ccbc6d41f2b411788afffbe4962a8a
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-21 16:00:28 +0700

    Create Class Product

---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace WeApp.Models
+public class Product
+{
+    public int ProductId { get; set; }
+    public short CategoryId { get; set; }
+    public string ProductName { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public short Quantity { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = null!;
+    public string Unit { get; set; } = null!;
+}
