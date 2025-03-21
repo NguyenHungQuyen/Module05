7378d2e0efb971f96fe3c9e3590b557e9faadd6a
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-21 11:36:39 +0700

    Class Product

--- WebApi/Models/Product.cs
+++ WebApi/Models/Product.cs
@@ -1,14 +1,14 @@
-using System;
-
-namespace WebApi.Models
+namespace WebApp.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+        public short CategoryId { get; set; }
         public string ProductName { get; set; } = null!;
         public string Description { get; set; } = null!;
         public short Quantity { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; } = null!;
+        public string Unit { get; set; } = null!;
     }
 }
