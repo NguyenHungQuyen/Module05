5d2bbbcccaf8a58f507d1753b6d681e797b69464
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-22 15:23:46 +0700

    Create API Controller GetProduct(page,size)

--- WebApi/Controllers/ProductController.cs
+++ WebApi/Controllers/ProductController.cs
@@ -17,5 +17,11 @@ namespace WebApi.Controllers
         {
             return Provider.Product.Add(obj);
         }
+        [HttpGet("{page}/{size}")]
+        [HttpGet("{page}")]
+        public IEnumerable<Product> GetProducts(int page, int size = 10)
+        {
+            return Provider.Product.GetProducts(page, size);
+        }
     }
 }
