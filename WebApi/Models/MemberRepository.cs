993d4a587910f991db2510a0a10feba8d2303dcc
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-27 08:44:35 +0700

    Function: int Add(RegisterModel)

--- WebApi/Models/MemberRepository.cs
+++ WebApi/Models/MemberRepository.cs
@@ -6,6 +6,19 @@ namespace WebApi.Models
 {
     public class MemberRepository : BaseRepository
     {
-        public MemberRepository(IDbConnection connection) : base(connection) { }
+        public MemberRepository(IDbConnection connection) : base(connection)
+        {
+        }
+        public int Add(RegisterModel obj)
+        {
+            return connection.Execute("AddMember", new
+            {
+                obj.MemberId,
+                obj.GivenName,
+                obj.Surname,
+                obj.Email,
+                Password = Helper.Hash(obj.Password)
+            }, commandType: CommandType.StoredProcedure);
+        }
     }
 }
