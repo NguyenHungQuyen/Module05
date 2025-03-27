8db1ef88e2d7885a36edea816ccc3f9f0a1fe245
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-27 09:00:43 +0700

    Write Function: int Register(RegisterModel)

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
 namespace WebApi.Controllers
 {
     public class AuthController : BaseController
     {
-        
+        [HttpPost("register")]
+        public int Register(RegisterModel obj)
+        {
+            return Provider.Member.Add(obj);
+        }
     }
 }
