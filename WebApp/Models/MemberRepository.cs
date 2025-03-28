5f7fac228876137cae06b33b123b73975bf5eb32
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-28 15:33:25 +0700

    Add Add method to MemberRepository

--- WebApp/Models/MemberRepository.cs
+++ WebApp/Models/MemberRepository.cs
@@ -6,4 +6,8 @@ public class MemberRepository : BaseRepository
     public MemberRepository(IConfiguration configuration) : base(configuration)
     {
     }
+    public int Add(RegisterModel obj)
+    {
+        return 0;
+    }
 }
