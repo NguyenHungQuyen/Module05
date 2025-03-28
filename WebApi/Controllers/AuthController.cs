44dceff3b22af1a3466f3ce1883d048b2a7f86cd
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-28 14:25:14 +0700

    Add register API in AuthController

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -3,6 +3,8 @@ using WebApi.Models;
 
 namespace WebApi.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : BaseController
     {
         [HttpPost("register")]
