34ea69b51ae0a2abca0d9d307b3fce25dedf0424
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-28 15:10:53 +0700

    feat(auth): Add Register method to AuthController

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers;
 
 public class AuthController:BaseController
 {
     public IActionResult Register()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterModel obj)
     {
         return View();
     }
