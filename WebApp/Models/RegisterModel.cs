a38ff996e1082771f0aac29d9917428cb627cc4c
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-28 14:28:51 +0700

    Add RegisterModel for user registration

---
+++ WebApp/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+namespace WebApp.Models;
+
+public class RegisterModel
+{
+    public string? MemberId { get; set; } 
+    public string GivenName { get; set; } = null!;
+    public string? Surname { get; set; }
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
