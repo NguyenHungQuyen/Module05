a10c661606922b40251cdbca655edeb814850ac5
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-29 15:21:47 +0700

    feat: Add basic login method in MemberRepository

--- WebApi/Models/MemberRepository.cs
+++ WebApi/Models/MemberRepository.cs
@@ -24,5 +24,9 @@ namespace WebApi.Models
                 Password = Helper.Hash(obj.Password)
             }, commandType: CommandType.StoredProcedure);
         }
+        public Member? Login(LoginModel obj)
+        {
+            return null;
+        }
     }
 }
